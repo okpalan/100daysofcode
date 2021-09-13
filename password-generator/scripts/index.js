f99f8f96ae514473d00b02cf6be6c5157d4b4e98
okpalan <89138089+okpalan@users.noreply.github.com>
2021-09-13 19:23:40 +0100

    A

--- password-generator/scripts/index.js
+++ password-generator/scripts/index.js
@@ -4,6 +4,6 @@
 const span = document.getElementById("password"),
   btn = document.getElementById("btn");
 const checkboxes = Array.from(document.querySelectorAll("input[type=checkbox]"));
-
+const length = document.getElementById("btn")
 const LETTERS = Array.from('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
 const SYMBOLS =Array.from('\'!"#$%&\\\{\/[{]_^({)+.@,-*.:?>=<')
