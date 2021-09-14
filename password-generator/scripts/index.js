5444c5f4ac290a239d04fdb24544b784975b2579
okpalan <89138089+okpalan@users.noreply.github.com>
2021-09-14 15:16:01 +0100

    A

--- password-generator/scripts/index.js
+++ password-generator/scripts/index.js
@@ -1,38 +1,16 @@
 "use strict";
 
 // constant symbols table of DOM Nodes;
-const password = document.getElementById("password"),
+const output = document.getElementById("psd"),
   btn = document.getElementById("btn");
 const checkboxes = Array.from(document.querySelectorAll("input[type=checkbox]"));
-
+const psdlength = document.getElementById("psd_len")
 const button = document.getElementById("btn")
 const LETTERS = Array.from('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
 const SYMBOLS =Array.from('\'!"#$%&\\{\/[{]_^({)+.@,-*.:?>=<')
-var password = ''
-        /* function updates the "output" element with the result from the
-"/update" url then calls itself again in 4 seconds
-*/
-function updater(){
-  var request = createXMLHttpRequest();
-  // add time to GET request to prevent caching issues
-  request.open("GET", "/" + (new Date()).getTime(), true);
-  request.onreadystatechange = function() {
-    if (request.readyState == 4) {
-        password.innerText = request.responseText;
-        setTimeout("updater()",4000);
-    }
-  }
-  request.send(null);
-}
+//Methods Utility programs
+const fill =Array.prototype.fill,
+random = Math.random;
 
-updater()
-/* cross browser way of creating XMLHttpRequests */
-function createXMLHttpRequest() {
-    if (typeof XMLHttpRequest != "undefined") {
-        return new XMLHttpRequest();
-    } else if (typeof ActiveXObject != "undefined") {
-        return new ActiveXObject("Microsoft.XMLHTTP");
-    } else {
-        throw new Error("XMLHttpRequest not supported");
-    }
-}
+var password = []
+
