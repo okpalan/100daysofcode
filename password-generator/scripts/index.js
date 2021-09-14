1c4ca43bd5966351defa6278a9c1c9828e36d5fc
okpalan <89138089+okpalan@users.noreply.github.com>
2021-09-14 16:03:35 +0100

    A

--- password-generator/scripts/index.js
+++ password-generator/scripts/index.js
@@ -1,16 +1,20 @@
 "use strict";
+const SYMBOLS = Array.from("'!\"#$%&\\{/[{]_^({)+.@,-*.:?>=<");
+//Methods Utility programs
+const fill = Array.prototype.fill,
+  random = Math.random,
+  from = Array.from;
 
 // constant symbols table of DOM Nodes;
-const output = document.getElementById("psd"),
+const output = document.getElementById("password"),
   btn = document.getElementById("btn");
-const checkboxes = Array.from(document.querySelectorAll("input[type=checkbox]"));
-const psdlength = document.getElementById("psd_len")
-const button = document.getElementById("btn")
-const LETTERS = Array.from('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-const SYMBOLS =Array.from('\'!"#$%&\\{\/[{]_^({)+.@,-*.:?>=<')
-//Methods Utility programs
-const fill =Array.prototype.fill,
-random = Math.random;
+const checkboxes = from(document.querySelectorAll("input[type=checkbox]"));
 
-var password = []
+const count = document.getElementById("count").value;
+const button = document.getElementById("btn");
+const LETTERS = from("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+const LOWERCASE = [...LETTERS.toString().toLowerCase()];
+var password = null;
 
+if (+count <= 4) alert("Password length cannot be less than 0");
+password = new Array(count).fill(0, 0);
